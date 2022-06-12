0b4d15e42699f1f3e0018ad03a7f0470f3f5fb08
robpduo <robpduo@Robs-MacBook-Pro.local>
2022-06-11 21:45:53 -0400

    6.2.3

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,15 @@
-var getUserRepos = function() {
-    console.log("function was called");
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
+
+// https://courses.bootcampspot.com/courses/2193/pages/6-dot-2-3-add-the-search-form?module_item_id=567592
 getUserRepos();
