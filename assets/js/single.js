f1d959d9f8740dd2d429cb3fdbad37b965cc52e8
robpduo <robpduo@Robs-MacBook-Pro.local>
2022-06-14 15:32:29 -0400

    proper repo link"
    ;
    q
    :wq
    "
    ""

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,25 +1,41 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if (repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 var getRepoIssues = function (repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
             response.json().then(function (data) {
-                // pass response data to dom function
                 displayIssues(data);
-            });
-            // check if api has paginated issues
-            if (response.headers.get("Link")) {
-                console.log("repo has more than 30 issues");
-            }
-        }
-        else {
-            alert("There was a problem with your request!");
-        }
 
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
+            });
+        } else {
+            // if not successful, redirect to homepage
+            document.location.replace("./index.html");
+        }
     });
 };
 
@@ -72,6 +88,5 @@ var displayWarning = function (repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
 
-https://courses.bootcampspot.com/courses/2193/pages/6-dot-4-3-create-a-link-between-pages?module_item_id=567643
